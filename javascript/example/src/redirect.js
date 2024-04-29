ee31b8e01b8b1f4d04e4002808a0d15876182b89
linan1109 <linan119@yeah.net>
2024-04-29 20:29:44 +0200

    update for web

--- javascript/example/src/redirect.js
+++ javascript/example/src/redirect.js
@@ -4,7 +4,7 @@ const url = new URL(location.href);
 const tokens = url.pathname.split(/[\\/]/g);
 const filename = tokens.pop();
 const parentDirectory = tokens[ tokens.length - 1 ];
-if (url.origin.includes('github') && parentDirectory !== 'bundle') {
-    url.pathname = tokens.join('/') + '/';
-    window.location.replace(new URL('bundle/' + filename, url.toString()));
-}
+// if (url.origin.includes('github') && parentDirectory !== 'bundle') {
+//     url.pathname = tokens.join('/') + '/';
+//     window.location.replace(new URL('bundle/' + filename, url.toString()));
+// }
