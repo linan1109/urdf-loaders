abff463e4d14b067508669f48b6d0ad5f8dac27c
linan1109 <linan119@yeah.net>
2024-06-23 21:17:40 +0200

    add drag on heatmap

--- javascript/src/utils/global-svg/global-heatmap-robot.js
+++ javascript/src/utils/global-svg/global-heatmap-robot.js
@@ -1,7 +1,5 @@
-import * as d3 from 'd3';
-import {globalHeatMapSVG} from './global-svg.js';
+import { globalHeatMapSVG } from './global-svg.js';
 import movementContainer from '../movement-container.js';
-import globalTimer from '../global-timer.js';
 
 export default class GlobalHeatmapRobot extends globalHeatMapSVG {
 
@@ -19,6 +17,7 @@ export default class GlobalHeatmapRobot extends globalHeatMapSVG {
         const dataLength = this.data.length;
         const eachGridDataLength = Math.floor(dataLength / this.gridNum);
         const processedData = [];
+        const yLabelOrder = {};
 
         this.yLabels.forEach((measurement, i) => {
             for (let j = 0; j < this.gridNum; j++) {
@@ -34,120 +33,11 @@ export default class GlobalHeatmapRobot extends globalHeatMapSVG {
                     value: sum / eachGridDataLength,
                 });
             }
+            yLabelOrder[measurement] = i;
         });
 
+        this.yLabelOrder = yLabelOrder;
         return processedData;
-    }
-
-    createHeatmap() {
-        const data = this.processData();
-        const numXLables = Math.floor(this.gridNum / 10);
-
-        const xLabels = Array.from({ length: numXLables }, (_, i) => i).map(
-            (d) => Math.floor((d * this.dataLength) / numXLables),
-        );
-
-        const yLabels = this.yLabels;
-
-        this.svg
-            .selectAll('.xLabel')
-            .data(xLabels)
-            .enter()
-            .append('text')
-            .text((d) => d)
-            .attr('x', (d, i) => i * this.gridWidth * 10)
-            .attr('y', 0)
-            .style('text-anchor', 'middle')
-            .attr('transform', `translate(${ this.gridWidth / 2 }, -6)`)
-            .attr('class', 'xLabel mono axis');
-
-        this.svg
-            .selectAll('.yLabel')
-            .data(yLabels)
-            .enter()
-            .append('text')
-            .text((d) => d)
-            .attr('x', 0)
-            .attr('y', (d, i) => i * this.gridHeight)
-            .style('text-anchor', 'end')
-            .attr('transform', `translate(-6, ${ this.gridHeight / 1.5 })`)
-            .attr('class', 'yLabel mono axis');
-
-        const cards = this.svg
-            .selectAll('.hour')
-            .data(data, (d) => `${ d.y }:${ d.x }`);
-
-        cards
-            .enter()
-            .append('rect')
-            .attr('x', (d) => d.x * this.gridWidth)
-            .attr('y', (d) => d.y * this.gridHeight)
-            // .attr('rx', 4)
-            // .attr('ry', 4)
-            .attr('class', 'hour bordered')
-            .attr('width', this.gridWidth)
-            .attr('height', this.gridHeight)
-            .merge(cards)
-            .transition()
-            .duration(1000)
-            .style('fill', (d) => this.colorScale(d.value));
-
-        cards.exit().remove();
-        const legends = [3, 2, 1, 0, -1, -2, -3];
-        const legend = this.svg.selectAll('.legend').data(legends);
-
-        const legendEnter = legend.enter().append('g').attr('class', 'legend');
-        const legendMargin = 20;
-        const heatmapWidth = this.width;
-        legendEnter
-            .append('rect')
-            .attr('x', heatmapWidth + legendMargin) // Position legend to the right of the heatmap
-            .attr('y', (d, i) => this.gridHeight * i) // Adjust vertical position based on index
-            .attr('width', this.gridWidth * 1.5)
-            .attr('height', this.gridHeight / 2)
-            .style('fill', (d, i) => this.colorScale(legends[i]));
-
-        legendEnter
-            .append('text')
-            .attr('class', 'mono')
-            .text((d) => ` ${ d }`)
-            .attr('x', heatmapWidth + legendMargin + this.gridWidth * 2) // Adjust x to align with the rect's right side
-            .attr('y', (d, i) => this.gridHeight * i + this.gridHeight / 2); // Center text vertically within the rect
-        legend.exit().remove();
-
-        // add brush
-        this.brush = d3
-            .brushX()
-            .extent([
-                [0, 0],
-                [this.width, this.height],
-            ])
-            .on('end', (event) => this.brushed(event));
-        this.svg.append('g').attr('class', 'brush').call(this.brush);
-
-        // bind events
-        this.svg
-            .on('click', (event) => this.singleclicked(event))
-            .on('pointermove', (event) => this.pointermoved(event))
-            .on('pointerleave', (event) => this.pointerleft(event));
-    }
-
-    updatePlotOnTime() {
-        // add the vertical line for current time
-        const current = globalTimer.getCurrent();
-        const x = current / this.dataLength;
-        this.drawlinebyx(x);
-
-        if (this.brushedWidth) {
-            const x0 = current - this.brushedWidth / 2;
-            const x1 = current + this.brushedWidth / 2;
-            this.svg
-                .selectAll('.brush')
-                .call(this.brush.move, [
-                    (x0 / this.dataLength) * this.width,
-                    (x1 / this.dataLength) * this.width,
-                ]);
-        }
     }
 
 }
--- javascript/src/utils/global-svg/global-svg.js
+++ javascript/src/utils/global-svg/global-svg.js
@@ -2,6 +2,7 @@ import globalVariables from '../global-variables.js';
 import globalTimer from '../global-timer.js';
 import animationControl from '../animation-control.js';
 import * as d3 from 'd3';
+import { or } from 'three/examples/jsm/nodes/Nodes.js';
 
 class globalPlotSVG {
 
@@ -128,13 +129,205 @@ class globalHeatMapSVG extends globalPlotSVG {
         // variables to set in child class
         this.dataLength = 0;
         this.brushedWidth = null;
-        
+
         this.colorScale = globalVariables.HeatmapColorScale;
 
         this.initSvg();
     }
 
     // functions inherite
+    createHeatmap() {
+        this.data = this.processData();
+        const numXLables = Math.floor(this.gridNum / 10);
+
+        const xLabels = Array.from({ length: numXLables }, (_, i) => i).map(
+            (d) => Math.floor((d * this.dataLength) / numXLables),
+        );
+
+        this.svg
+            .selectAll('.xLabel')
+            .data(xLabels)
+            .enter()
+            .append('text')
+            .text((d) => d)
+            .attr('x', (d, i) => i * this.gridWidth * 10)
+            .attr('y', 0)
+            .style('text-anchor', 'middle')
+            .attr('transform', `translate(${ this.gridWidth / 2 }, -6)`)
+            .attr('class', 'xLabel mono axis');
+
+        this.labelContainer = this.svg
+            .append('g')
+            .attr('class', 'label-container');
+        this.yLabelGroups = this.labelContainer
+            .selectAll('.yLabelGroup')
+            .data(this.yLabels.map((label, index) => ({ label, index })))
+            .enter()
+            .append('g')
+            .attr('class', 'yLabelGroup')
+            .attr(
+                'transform',
+                (d) => `translate(0, ${ d.index * this.gridHeight })`,
+            )
+            .call(
+                d3
+                    .drag()
+                    .on('drag', (event, d) => {
+                        const newY = Math.min(
+                            Math.max(0, d3.pointer(event, this.svg.node())[1]),
+                            (this.yLabels.length - 1) * this.gridHeight,
+                        );
+                        d3.select(event.sourceEvent.target.parentNode).attr(
+                            'transform',
+                            `translate(0, ${ newY })`,
+                        );
+                    })
+                    .on('end', (event, d) => {
+                        const newY = Math.min(
+                            Math.max(0, d3.pointer(event, this.svg.node())[1]),
+                            (this.yLabels.length - 1) * this.gridHeight,
+                        );
+
+                        const newYIndex = Math.floor(newY / this.gridHeight);
+                        const selectedLabel = this.yLabels[d.index];
+                        // remove the yLabel from the YLabels
+                        this.yLabels.splice(d.index, 1);
+                        // insert the yLabel to the newYIndex
+                        this.yLabels.splice(newYIndex, 0, selectedLabel);
+
+                        console.log('yLabels', this.yLabels);
+
+                        this.updateHeatmap();
+                    }),
+            );
+
+        this.yLabelGroups
+            .append('text')
+            .text((d) => d.label)
+            .attr('x', 0)
+            .attr('y', this.gridHeight / 2)
+            .style('text-anchor', 'end')
+            .attr('transform', `translate(-6, ${ this.gridHeight / 2 })`)
+            .attr('class', 'yLabel mono axis');
+
+        this.yLabelGroups.each((d, i, nodes) => {
+            const smallHeatmap = this.createSmallHeatmap(d.label, this.data, i);
+            d3.select(nodes[i]).node().appendChild(smallHeatmap);
+        });
+
+        const legends = [3, 2, 1, 0, -1, -2, -3];
+        const legend = this.svg.selectAll('.legend').data(legends);
+
+        const legendEnter = legend.enter().append('g').attr('class', 'legend');
+        const legendMargin = 20;
+        const heatmapWidth = this.width;
+        legendEnter
+            .append('rect')
+            .attr('x', heatmapWidth + legendMargin) // Position legend to the right of the heatmap
+            .attr('y', (d, i) => this.gridHeight * i) // Adjust vertical position based on index
+            .attr('width', this.gridWidth * 1.5)
+            .attr('height', this.gridHeight / 2)
+            .style('fill', (d, i) => this.colorScale(legends[i]));
+
+        legendEnter
+            .append('text')
+            .attr('class', 'mono')
+            .text((d) => ` ${ d }`)
+            .attr('x', heatmapWidth + legendMargin + this.gridWidth * 2) // Adjust x to align with the rect's right side
+            .attr('y', (d, i) => this.gridHeight * i + this.gridHeight / 2); // Center text vertically within the rect
+        legend.exit().remove();
+
+        // add brush
+        this.brush = d3
+            .brushX()
+            .extent([
+                [0, 0],
+                [this.width, this.height],
+            ])
+            .on('end', (event) => this.brushed(event));
+        this.svg.append('g').attr('class', 'brush').call(this.brush);
+
+        // bind events
+        this.svg
+            .on('click', (event) => this.singleclicked(event))
+            .on('pointermove', (event) => this.pointermoved(event))
+            .on('pointerleave', (event) => this.pointerleft(event));
+    }
+
+    createSmallHeatmap(ylabel, allData, orderNow) {
+        const data = allData.filter((d) => d.y === this.yLabelOrder[ylabel]);
+
+        const svg = d3
+            .create('svg')
+            .attr('width', this.gridWidth)
+            .attr('height', this.gridHeight)
+            .attr('viewBox', [0, 0, this.gridWidth, this.gridHeight])
+            .attr(
+                'style',
+                'max-width: 100%; height: auto; overflow: visible; font: 10px sans-serif;',
+            );
+
+        const cards = svg
+            .selectAll('.cards')
+            .data(data, (d) => `${ d.y }:${ d.x }`);
+
+        cards
+            .enter()
+            .append('rect')
+            .attr('x', (d) => d.x * this.gridWidth)
+            .attr('y', 0)
+            .attr('class', 'card bordered')
+            .attr('width', this.gridWidth)
+            .attr('height', this.gridHeight)
+            .style('fill', (d) => this.colorScale(d.value));
+
+        cards.exit().remove();
+        return svg.node();
+    }
+
+    updateHeatmap() {
+        console.log('updateHeatmap');
+        console.log('yLabels', this.yLabels);
+
+        // update yLabel
+        this.yLabelGroups = this.labelContainer
+            .selectAll('.yLabelGroup')
+            .data(this.yLabels.map((label, index) => ({ label, index })));
+        this.yLabelGroups
+            .transition()
+            .duration(500)
+            .attr(
+                'transform',
+                (d) => `translate(0, ${ d.index * this.gridHeight })`,
+            );
+
+        this.yLabelGroups
+            .select('text')
+            .text((d) => d.label)
+            .attr('y', this.gridHeight / 2);
+
+        this.yLabelGroups.each((d, i, nodes) => {
+            const smallHeatmap = this.createSmallHeatmap(d.label, this.data, i);
+            d3.select(nodes[i]).node().appendChild(smallHeatmap);
+        });
+    }
+    updatePlotOnTime() {
+        // add the vertical line for current time
+        const current = globalTimer.getCurrent();
+        const x = current / this.dataLength;
+        this.drawlinebyx(x);
+
+        if (this.brushedWidth) {
+            const x0 = current - this.brushedWidth / 2;
+            const x1 = current + this.brushedWidth / 2;
+            this.svg
+                .selectAll('.brush')
+                .call(this.brush.move, [
+                    (x0 / this.dataLength) * this.width,
+                    (x1 / this.dataLength) * this.width,
+                ]);
+        }
+    }
 
 }
 
